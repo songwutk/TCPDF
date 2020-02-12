ad7abfa97c68f9fe00e80d5f575a9aef0ee21100
songwutk <songwutk@gmail.com>
2020-02-12 11:49:11 +0700

    Create th.php

---
+++ examples/lang/th.php
@@ -0,0 +1,45 @@
+<?php
+//============================================================+
+// File name   : th.php
+// Begin       : 2020-02-12
+// Last Update : 2020-02-12
+//
+// Description : Language module for TCPDF
+//               (contains translated texts)
+//               Thai
+//
+// Author: Songwut Kanchanakosai
+//
+// (c) Copyright:
+//               Nicola Asuni
+//               Tecnick.com LTD
+//               www.tecnick.com
+//               info@tecnick.com
+//============================================================+
+
+/**
+ * TCPDF language file (contains translated texts).
+ * @package com.tecnick.tcpdf
+ * @brief TCPDF language file: Thai
+ * @author Songwut Kanchanakosai
+ * @since 2020-02-12
+ */
+
+// Thai
+
+global $l;
+$l = Array();
+
+// PAGE META DESCRIPTORS --------------------------------------
+
+$l['a_meta_charset'] = 'UTF-8';
+$l['a_meta_dir'] = 'ltr';
+$l['a_meta_language'] = 'th';
+
+// TRANSLATIONS --------------------------------------
+$l['w_page'] = 'หน้า';
+//define ('PDF_FONT_NAME_DATA', 'freeserif');
+
+//============================================================+
+// END OF FILE
+//============================================================+
